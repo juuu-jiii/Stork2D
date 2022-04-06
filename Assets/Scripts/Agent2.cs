fa794d22f286cc1f6cf8a981d1113f8b05c52cec
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2022-04-06 18:20:36 -0400

    Added documentation to Agent2.

--- Assets/Scripts/Agent2.cs
+++ Assets/Scripts/Agent2.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Adapted from:
+/// - https://github.com/beneater/boids/
+/// - https://github.com/zklinger2000/unity-boids
+/// - https://www.oreilly.com/library/view/ai-for-game/0596005555/ch04.html
+/// </summary>
 public class Agent2 : MonoBehaviour
 {
-    public bool log;
+    //public bool log;
+
+    // Property separate from the field because Vector3's are structs. This one
+    // in particular needs to be modified within the class, and so a returned
+    // copy (if an auto-property were used) is not very helpful.
+
+    /// <summary>
+    /// This agent's velocity vector.
+    /// </summary>
     private Vector3 velocity;
+
+    /// <summary>
+    /// Returns a copy of this agent's velocity vector.
+    /// </summary>
     public Vector3 Velocity { get { return velocity; }}
+
+    /// <summary>
+    /// Sprite used to represent this agent onscreen.
+    /// </summary>
     private Sprite sprite;
 
     /// <summary>
@@ -30,6 +52,10 @@ public class Agent2 : MonoBehaviour
     /// </summary>
     private Vector3 totalVelocity;
 
+    /// <summary>
+    /// Running total of distance vectors from this agent to its neighbours 
+    /// that are too close by.
+    /// </summary>
     private Vector3 totalAvoidance;
 
     /// <summary>
@@ -47,20 +73,48 @@ public class Agent2 : MonoBehaviour
     /// </summary>
     private Vector3 sceneDimensions;
 
-    public float visualRange;
-    public float minDistance;
+    /// <summary>
+    /// Radius of this agent's field of view.
+    /// </summary>
+    [SerializeField] private float visualRange;
 
-    public float cohesionFactor;
-    public float alignmentFactor;
-    public float separationFactor;
+    /// <summary>
+    /// The minimum distance this agent should strive to be from all others in
+    /// its vicinity. 
+    /// Expressed as a factor of the agent's length.
+    /// </summary>
+    [SerializeField] private float minDistance;
+
+    /// <summary>
+    /// Influence of the cohesion rule.
+    /// </summary>
+    [SerializeField] private float cohesionFactor;
+
+    /// <summary>
+    /// Influence of the alignment rule.
+    /// </summary>
+    [SerializeField] private float alignmentFactor;
+
+    /// <summary>
+    /// Influence of the separation rule.
+    /// </summary>
+    [SerializeField] private float separationFactor;
 
     /// <summary>
     /// The strength of the force used to keep an agent on-screen.
     /// </summary>
     [SerializeField] private float screenRetentionFactor;
 
-    public float margin;
-    public float maxSpeed;
+    /// <summary>
+    /// Distance from the edge of the screen the agent must keep within.
+    /// </summary>
+    [SerializeField] private float margin;
+
+    /// <summary>
+    /// The maximum speed this agent can travel at.
+    /// </summary>
+    [SerializeField] private float maxSpeed;
+
     [SerializeField] private float maxAngle;
 
     // Start is called before the first frame update
@@ -68,16 +122,23 @@ public class Agent2 : MonoBehaviour
     {
         sprite = GetComponent<SpriteRenderer>().sprite;
         length = GetComponent<Renderer>().bounds.size.y;
-        sceneDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 13));
+
+        // Spawn agents at random locations and velocities onscreen.
+        sceneDimensions = Camera.main.ScreenToWorldPoint(
+            new Vector3(Screen.width, Screen.height, 13));
         transform.position = new Vector3(
             Random.Range(-sceneDimensions.x / 2, sceneDimensions.x / 2),
             Random.Range(-sceneDimensions.y / 2, sceneDimensions.y / 2),
             0);
-        transform.rotation = Quaternion.LookRotation(Velocity.normalized);
-
         velocity = new Vector3(Random.Range(1, 6), Random.Range(1, 6), 0);
+
+        // Idk
+        //transform.rotation = Quaternion.LookRotation(Velocity.normalized);
     }
 
+    /// <summary>
+    /// Neighbour-checking logic.
+    /// </summary>
     private void CheckNeighbours()
     {
         //float signedAngle;
@@ -134,6 +195,10 @@ public class Agent2 : MonoBehaviour
                 //        || (localDistance.y < 0 && 
                 //}
 
+                // If in visual range, the agent is considered a neighbour to
+                // this one, and included in subsequent calculations.
+                // Using a function of the agent's length so the simulation
+                // scales along with world size.
                 if (distance < length * visualRange)
                 {
                     totalPosition += a.transform.position;
@@ -142,6 +207,8 @@ public class Agent2 : MonoBehaviour
                     neighbourCount++;
                 }
 
+                // If close enough, the agent presents a potential collision,
+                // and is included in separation calculations.
                 if (distance < length * minDistance)
                 {
                     totalAvoidance += distanceVector;
@@ -150,29 +217,54 @@ public class Agent2 : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Applies cohesion rule calculations.
+    /// </summary>
     private void CalculateCohesion()
     {
+        // Obtain average position and alter velocity with the help of 
+        // cohesionFactor.
         avgPosition = totalPosition / neighbourCount;
 
         velocity.x += (avgPosition.x - transform.position.x) * cohesionFactor;
         velocity.y += (avgPosition.y - transform.position.y) * cohesionFactor;
     }
 
+    /// <summary>
+    /// Applies alignment rule calculations.
+    /// </summary>
     private void CalculateAlignment()
     {
+        // Obtain average velocity and alter velocity with the help of 
+        // alignmentFactor.
         avgVelocity = totalVelocity / neighbourCount;
 
         velocity.x += (avgVelocity.x - velocity.x) * alignmentFactor;
         velocity.y += (avgVelocity.y - velocity.y) * alignmentFactor;
     }
 
+    /// <summary>
+    /// Applies separation rule calculations.
+    /// </summary>
     private void CalculateSeparation()
     {
         velocity += totalAvoidance * separationFactor;
     }
 
+    /// <summary>
+    /// Keeps the agent onscreen.
+    /// </summary>
     private void RetainOnScreen()
     {
+        // Viewing the sprite as an AABB, obtain its four bounds.
+        float spriteUpperBound = Camera.main.WorldToScreenPoint(
+            transform.position - new Vector3(0, sprite.bounds.extents.y, 0)).y;
+        float spriteLowerBound = Camera.main.WorldToScreenPoint(
+            transform.position + new Vector3(0, sprite.bounds.extents.y, 0)).y;
+        float spriteLeftBound = Camera.main.WorldToScreenPoint(
+            transform.position - new Vector3(sprite.bounds.extents.x, 0, 0)).x;
+        float spriteRightBound = Camera.main.WorldToScreenPoint(
+            transform.position + new Vector3(sprite.bounds.extents.x, 0, 0)).x;
         //// ----- USE THIS -----
         //if (transform.position.x < -sceneDimensions.x / 2)
         //    newVelocity.x += screenRetentionFactor;
@@ -186,26 +278,28 @@ public class Agent2 : MonoBehaviour
 
         //screenPos = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (Camera.main.WorldToScreenPoint(transform.position - new Vector3(0, sprite.bounds.extents.y, 0)).y < margin)
+        // Screen coordinates are 2D and measured in pixels. They start in the
+        // lower left corner (0, 0) and go to (Screen.width, Screen.height).
+        if (spriteUpperBound < margin)
         {
             velocity.y += screenRetentionFactor;
             //velocity.y *= -1;
             //velocity = -velocity;
         }
-        if (Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, sprite.bounds.extents.y, 0)).y > Screen.height - margin)
+        if (spriteLowerBound > Screen.height - margin)
         {
             velocity.y -= screenRetentionFactor;
             //velocity.y *= -1;
             //velocity = -velocity;
         }
 
-        if (Camera.main.WorldToScreenPoint(transform.position - new Vector3(sprite.bounds.extents.x, 0, 0)).x < margin)
+        if (spriteLeftBound < margin)
         {
             velocity.x += screenRetentionFactor;
             //velocity.x *= -1;
             //velocity = -velocity;
         }
-        if (Camera.main.WorldToScreenPoint(transform.position + new Vector3(sprite.bounds.extents.x, 0, 0)).x > Screen.width - margin)
+        if (spriteRightBound > Screen.width - margin)
         {
             velocity.x -= screenRetentionFactor;
             //velocity.x *= -1;
@@ -213,6 +307,9 @@ public class Agent2 : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Prevents the agent from travelling too fast.
+    /// </summary>
     private void LimitSpeed()
     {
         //Rb.velocity = Rb.velocity.normalized * maxSpeed;
@@ -236,23 +333,31 @@ public class Agent2 : MonoBehaviour
 
         CheckNeighbours();
 
+        // Cohesion and alignment rules depend on the presence of neighbours.
         if (neighbourCount > 0)
         {
             CalculateCohesion();
             CalculateAlignment();
         }
         
+        // Separation does not necessarily depend on the presence of neighbours;
+        // visual range != minimum distance.
         CalculateSeparation();
         
+        // Housekeeping
         LimitSpeed();
         //LimitRotation();
         RetainOnScreen();
         
+        // Apply calculated changes to this agent.
         transform.position = new Vector3(
             transform.position.x + velocity.x,
             transform.position.y + velocity.y,
             transform.position.z);
+
+        // Idk??
+        transform.rotation = Quaternion.LookRotation(transform.forward);
     }
 }
 
-// TODO: reset neighbourCount
+// TODO: rotate agent
