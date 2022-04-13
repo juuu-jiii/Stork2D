979c607045d6095950f505f875d2c0fb3c929ef5
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2022-04-12 23:47:25 -0400

    Added cursor tracking.

---
+++ Assets/Scripts/CursorTracker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorTracker : MonoBehaviour
+{
+    Vector3 cursorPosScreen;
+    Vector3 cursorPosWorld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TrackCursor();
+    }
+
+    private void TrackCursor()
+    {
+        // Input.mousePosition's z-value is always 0. Set it to reflect the
+        // Camera's height from the ground for accurate tracking of mouse
+        // click positions.
+        cursorPosScreen = Input.mousePosition;
+        cursorPosScreen.z = Camera.main.transform.position.z;
+
+        // Input.mousePosition returns data in terms of screen coordinates.
+        // Convert this to world coordinates.
+        cursorPosWorld = Camera.main.ScreenToWorldPoint(cursorPosScreen);
+
+        Debug.Log(cursorPosWorld);
+    }
+}
